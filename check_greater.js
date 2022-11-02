3e27363f84327f41fea095bb8a579c6956da729f
gopi vishwakarma <g2468vi@gmail.com>
2022-11-02 12:05:14 +0530

    code fixed by vishwakarma

--- check_greater.js
+++ check_greater.js
@@ -1,8 +1,15 @@
-function checkNumber(a,b){
-     if(a>b){
+function checkNumber(a,b,c){
+     if(a>b&&a>c){
       console.log("a is greater")
 }
-else{
+else if(b>a&&b>c){
 console.log("b is greater")
 }
+
+else{
+console.log("c is greater")
 }
+}
+
+let answer=checkNumber(4,5,6)
+console.log(answer)
